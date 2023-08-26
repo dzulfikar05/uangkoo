f070a784ca876c63caa3bad98825d5106a998086
dzulfikarmagang123 <108442440+dzulfikarmagang123@users.noreply.github.com>
2023-08-26 22:10:14 +0700

    CRUD category

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_appbar/calendar_appbar.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:uangkoo/pages/category_page.dart';
 import 'package:uangkoo/pages/home_page.dart';
+import 'package:uangkoo/pages/transaction_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -10,29 +14,53 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Widget> _children = [HomePage(), CategoryPage()];
+  int currentIndex = 0;
+
+   void onTapTapped(int index){
+    setState(() {
+      currentIndex = index;
+    });
+   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // Calender Top
-      appBar: CalendarAppBar(
+      appBar: (currentIndex == 0)?CalendarAppBar(
         backButton: false,
         locale: 'id',
         accent: Colors.green,
         onDateChanged: (value) => print(value),
         firstDate: DateTime.now().subtract(Duration(days: 140)),
         lastDate: DateTime.now(),
+      ) : PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
+            child: Text("Categories", style: GoogleFonts.montserrat(fontSize: 20),),
+          ),
+        )
       ),
 
       // Body
-      body: HomePage(),
+      body: _children[currentIndex],
 
       // Bottom Nav
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
-        },
-        backgroundColor: Colors.green, 
-        child: Icon(Icons.add),
+      floatingActionButton: Visibility(
+        visible: (currentIndex == 0) ? true : false,
+        child: FloatingActionButton(
+          onPressed: (){
+            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>TransactionPage())).then((value){
+              setState(() {
+                
+              });
+            });
+          },
+          backgroundColor: Colors.green, 
+          child: Icon(Icons.add),
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
@@ -40,14 +68,18 @@ class _MainPageState extends State<MainPage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,  
           children: [
             IconButton(
-              onPressed: (){}, 
+              onPressed: (){
+                onTapTapped(0);
+              }, 
               icon: Icon(Icons.home)
             ),
             SizedBox(
               width: 20,
             ),
             IconButton(
-              onPressed: (){}, 
+              onPressed: (){
+                onTapTapped(1);
+              }, 
               icon: Icon(Icons.list)
             ),
           ]
